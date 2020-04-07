49250622895a88393cd16e819766dbefb078c408
eoghanb6 <eoghan.bradley@kainos.com>
2020-04-07 19:47:49 +0100

    can move items to list and now theres an image for no recipes

--- pantryManager/src/app/modal/modal.page.ts
+++ pantryManager/src/app/modal/modal.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NavParams, ModalController } from '@ionic/angular';
+import { AngularFirestore } from '@angular/fire/firestore'
+import { UserService } from '../user.service'
+import { firestore } from 'firebase/app'
 
 @Component({
   selector: 'app-modal',
@@ -9,12 +12,22 @@ import { NavParams, ModalController } from '@ionic/angular';
 export class ModalPage implements OnInit {
 
   recipeInfo
-
+  userIngredients
+  shoppingListArr = []
 
   constructor(public modalController: ModalController,
-  navParams: NavParams) {
+  navParams: NavParams, public afstore: AngularFirestore,
+        public user: UserService) {
 console.log(navParams.get('recipeInfo'));
 this.recipeInfo = navParams.get('recipeInfo');
+const ingreds = afstore.doc(`users/${user.getUID()}`)
+this.userIngredients = ingreds.valueChanges()
+this.userIngredients.subscribe( data => {
+    for (let ingredient of data.shoppingList) {
+      this.shoppingListArr.push(ingredient.ingredientName);
+    }
+  }
+)
   }
 
   ngOnInit() {
@@ -22,4 +35,26 @@ this.recipeInfo = navParams.get('recipeInfo');
 
 
 closeModal() { this.modalController.dismiss(); }
+
+
+addToList(name, amount, unit){
+  console.log("adding ingredient to  shopping list")
+      const ingredientName = name;
+      const ingredientQuantity = amount + " " + unit;
+
+      //add ingredient to firestore here for this user
+     this.afstore.doc(`users/${this.user.getUID()}`).update({
+       shoppingList: firestore.FieldValue.arrayUnion({
+         ingredientName,
+         ingredientQuantity
+       })
+     })
+}
+
+inShoppingList(name){
+  console.log("checking user ingredients")
+  console.log(this.shoppingListArr)
+  return (this.shoppingListArr.indexOf(name) > -1);
+
+}
 }
